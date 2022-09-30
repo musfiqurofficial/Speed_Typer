773ff04fbc8de81919d2982a3d8e052c6f34fb31
musfiqurofficial <musfiqurofficial1999@gmail.com>
2022-09-30 20:46:44 +0600

    Comment

--- src/main/Qus/Qus.js
+++ src/main/Qus/Qus.js
@@ -8,7 +8,7 @@ const Qus = () => {
                 <h2 className='text-2xl font-bold text-amber-300'>01. How dose React Work?</h2>
                 <p ><span className='text-xl font-bold text-amber-400'>Ans:</span> <span className='text-xl font-semibold'>R</span>eact is a JavaScript library (not a framework) that creates user interfaces (UIs) in a predictable and efficient way using declarative code. You can use it to help build single page applications and mobile apps, or to build complex apps if you utilise it with other libraries.</p>
             </div>
-            {/* Difference between props and state. */}
+            {/* Difference between props and state? */}
             <div className='mb-10 bg-zinc-800 text-white p-8 rounded-lg '>
                 <h2 className='text-2xl font-bold text-amber-300'>02. Difference between props and state.</h2>
                 <p><span className='text-xl font-bold text-amber-400'>Ans:</span> <span className='text-xl font-semibold'>P</span>rops are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component.</p>
